dcf5f343229707676dd9a2e3626a73cbb6b036d2
Mukesh <jais.mukesh@gmail.com>
2023-01-31 14:49:07 +0100

    feat: tesing url_for

--- hello.py
+++ hello.py
@@ -1,21 +1,34 @@
 from flask import Flask
+from flask import url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index Page"
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    # show the user profile for that user
+    return f'{username}\'s profile'
+
+# Test
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+
 
 @app.route('/hello')
 def hello():
     return 'Hello, WOrld'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
